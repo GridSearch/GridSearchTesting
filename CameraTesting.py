2b08ad036741e6565d6739c927edd0da3194cdd8
Logan <lwmcdona@ualberta.ca>
2017-10-23 17:30:26 -0600

    Trying to commit again

--- CameraTesting.py
+++ CameraTesting.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 
+# Logan was here
+
 cap = cv2.VideoCapture(0)
 
 while(True):
@@ -12,14 +14,14 @@ while(True):
 
     # Display the resulting frame
 
-    frame[100,100] = [0,0,0]				#draws a small straight black 
+    frame[100,100] = [0,0,0]				#draws a small straight black
     frame[101,100] = [0,0,0]				#line where pixels are
     frame[102,100] = [0,0,0]
-    
+
    # for x in range(260,460):
     #    for y in range(540,740):
      #       gray[x,y] = [0,0,0]
-     
+
 
 
     cv2.imshow('frame',gray)
@@ -27,7 +29,7 @@ while(True):
     cv2.namedWindow("frame", 0)
     #cv2.resizeWindow("frame", 1000,1000)   #resize image, not really useful
     cv2.moveWindow('frame', 0, 0)      #Moves window to top left of screen
-    
+
     #cv2.imwrite('max.png',gray)  #saves image
     print gray.shape	#image properties [rows columns channels]
     if cv2.waitKey(1) & 0xFF == ord('q'):	#waitkey does not work
