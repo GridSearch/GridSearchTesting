33c5524272c612fe30e00d1f78b3f00e9095ee10
Max McEvoy <maxmcevoy@MacBook-Pro.local>
2017-11-08 15:48:01 -0700

    Added boundary boxes around grid

--- VideoTesting.py
+++ VideoTesting.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import math
 #Tests isolating red leds and blue boxs colors in buff_test_video_01.mpeg video file
 
 cap = cv2.VideoCapture("buff_test_video_01.mpeg")
@@ -12,15 +13,19 @@ while(True):
 
 
     # Display the resulting frame
-#******************************************* Resized imaged shown to be smaller
+#*******************************************
+#Resized imaged shown to be smaller
     r = 500.0 / frame.shape[1]
     dim = (500, int(frame.shape[0] * r))
     frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+    Nframe = frame
 #*******************************************
 
     convertedimage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 #*******************************************
+#Isolate colors Red and Blue
+
 #define range of blue color in HSV
     lower_blue = np.array([100,50,50])
     upper_blue = np.array([130,255,255])
@@ -31,7 +36,6 @@ while(True):
     upper_red = np.array([100, 255, 255])
         #Adjust accordingly
 
-
 # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(convertedimage, lower_red, upper_red)
     mask2 = cv2.inRange(convertedimage, lower_blue, upper_blue)
@@ -41,7 +45,7 @@ while(True):
     res = cv2.bitwise_and(frame,frame, mask=mask)
     res2 = cv2.bitwise_and(frame,frame, mask=mask2)
 
-    cv2.imshow('Original',frame)
+#cv2.imshow('Original',frame)
     cv2.moveWindow('Original', 0, 0)
 
     cv2.imshow('RED_LED',res)
@@ -49,13 +53,6 @@ while(True):
 
     cv2.imshow('Blue_Boxes',res2)
     cv2.moveWindow('Blue_Boxes', 0, 350)
-
-    cv2.moveWindow('frame', 0, 0)      #Moves window to top left of screen
-
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27: #ESC  to quit
-        print "Quitting"
-        break
 
 #*******************************************
 
@@ -65,24 +62,37 @@ while(True):
 
 #*******************************************
 #Red Contour lines
-
     ret,thresh = cv2.threshold(mask2,127,255,1)
 
     im2,contours,h = cv2.findContours(thresh,1,2)
 
-    cv2.drawContours(frame, contours, -1, (0,0,255), 2)
+    cv2.drawContours(frame, contours, -1, (0,0,255), 1)
 
-    cv2.imshow('Contours',frame)
+    cv2.imshow('Contours',frame) #having difficulties displaying contour lines on this window and not the original frame
     cv2.moveWindow('Contours',500,350)
+#*******************************************
+#White Boundary Boxes
+    for contour in contours:
+        tempRect = cv2.minAreaRect(contour) #returns the [(x,y),(width,height),(rotation)] of contour lines
 
+        width = int(tempRect[1][0])
+        height = int(tempRect[1][1])
+        x = int(tempRect[0][0])
+        y = int(tempRect[0][1])
+        
+        if (width > 25) & (width < 35) & (height > 14) & (height < 30): #roughly the size of our boxes
+            cv2.rectangle(frame,(x-14,y-8),(width+x-14,height+y-8),(255,255,0),1) #x,y seem to be off by a factor??? why?
+#            print "width: %f " %width  #Helpful figuring out the right approximated heights and width of boxes
+#            print " height: %f"  %height
 
-
-    tempRec = cv2.minAreaRect(contours)
-    width = tempRec[0][1]
-    height = tempRec[1][1]
-
+    cv2.imshow('Original',frame)
 #*******************************************
 #captures image of frame then quits
+
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27: #ESC  to quit
+        print "Quitting"
+        break
     if cv2.waitKey(1) & 0xFF == ord('a'):
         cv2.imwrite('BlueGrid.png',frame)
         print "captured image"
